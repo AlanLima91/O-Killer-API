50fc04bab4309f6883b47f2544ddefd59ad07ede
Alan <m.limaalan@hotmail.fr>
2019-08-14 15:11:09 +0200

    add public folder and body parser

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ const express       = require('express');
 
 var app = express();
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.status(200).send('Server listening !')
 })
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(8000, () => {
     console.log('Listening on port 8000');
-    
 })
 
 module.exports = {app}
