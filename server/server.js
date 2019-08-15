427a1b963a3e0f42885c00363565346516d9ea85
Alan <m.limaalan@hotmail.fr>
2019-08-15 18:26:54 +0200

    add CORS

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Expose-Headers', 'X-Auth');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+  })
+
 require('./controllers/controllersLoader')(app);
 
 app.get('/', (req, res) => {
