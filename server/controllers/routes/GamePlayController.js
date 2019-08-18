cf24b2482569713bad2e1171aa1a3e8d6ce0468b
Alan <m.limaalan@hotmail.fr>
2019-08-19 01:33:19 +0200

    change gamplay path

--- server/controllers/routes/GamePlayController.js
+++ server/controllers/routes/GamePlayController.js
@@ -2,7 +2,7 @@ const GamePlay  = require('../lib/gameplay');
 
 module.exports = function (app) {
 	
-	app.get('/gameplay/all', GamePlay.getAllGamePlay);
+	app.get('/gameplays', GamePlay.getAllGamePlay);
 
 	app.get('/gameplay/:id', GamePlay.getGamePlay);
 
