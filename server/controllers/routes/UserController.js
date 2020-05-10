26ec905d14b42ce09d2e241ae1ea2ea163c8867a
Alan <alan@hypecode.com>
2020-05-10 23:33:39 +0200

    update users routes

--- server/controllers/routes/UserController.js
+++ server/controllers/routes/UserController.js
@@ -1,25 +1,24 @@
-const User  = require('../lib/user');
+const User = require('../lib/user')
 
 module.exports = function (app) {
-    
-    // POST /register
-    app.post('/register', User.addUser)
+  // POST /register
+  app.post('/register', User.addUser)
 
-    // GET /users
-    app.get('/user/all', User.getUsers)
+  // GET /users
+  app.get('/users', User.getUsers)
 
-    // GET /user/id
-    app.get('/user/:id', User.getUser)
+  // GET /user/id
+  app.get('/users/:id', User.getUser)
 
-    // PATCH /user/id
-    app.patch('/user/:id', User.patchUser)
+  // PATCH /user/id
+  app.patch('/users/:id', User.patchUser)
 
-    // DELETE /user/id
-    app.delete('/user/:id', User.deleteUser)
+  // DELETE /user/id
+  app.delete('/users/:id', User.deleteUser)
 
-    // LOGIN /login
-    app.post('/login', User.login)
-    
-    // LOGINJWT
-    app.post('/loginjwt', User.loginJWT)
+  // LOGIN /login
+  app.post('/login', User.login)
+
+  // LOGINJWT
+  app.post('/loginjwt', User.loginJWT)
 }
